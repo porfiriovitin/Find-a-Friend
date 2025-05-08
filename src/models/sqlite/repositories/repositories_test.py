4fca01ba1ed760d8fe2699c2fc18cd4c304b6f5d
porfiriovitin <victorhugo.svm@hotmail.com>
2025-05-07 21:36:30 -0300

    config: making the db connection engine  and integration tests

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,17 @@
+import pytest
+from src.models.sqlite.settings.connection import dbconnectionhandler
+from .pets_repository import PetsRepository
+
+dbconnectionhandler.connect_to_db()
+
+@pytest.mark.skip(reason="db iter")
+def test_list_pets():
+    repo = PetsRepository(db_connection=dbconnectionhandler)
+    response = repo.list_pets()
+    print(response)
+
+@pytest.mark.skip(reason="db iter")
+def test_delete_pet():
+    pet_name = "cobra"
+    repo = PetsRepository(db_connection=dbconnectionhandler)
+    repo.delete_pets(pet_name)
